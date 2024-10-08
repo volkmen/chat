a61d10bd5b972fe80722bcd2359c08070bb97bc0
yar <yaroslvovk@gmail.com>
2024-10-08 09:49:02 +0300

    commit

---
+++ convertGraphql2Types.js
@@ -0,0 +1,13 @@
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { mergeTypeDefs } = require('@graphql-tools/merge');
+const { print } = require('graphql');
+const path = require('path');
+const fs = require('fs');
+
+const loadedFiles = loadFilesSync(path.join(__dirname, 'src'), {
+  extensions: ['.graphql']
+});
+const typeDefs = mergeTypeDefs(loadedFiles);
+const printedTypeDefs = print(typeDefs);
+
+fs.writeFileSync('src/schema.graphql', printedTypeDefs);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from '@apollo/server'; // preserve-line
 import { startStandaloneServer } from '@apollo/server/standalone'; // preserve-line
 import { mergedSchema } from './api';
+import { Connection } from 'mongoose';
 
 class App {
   private server: ApolloServer;
@@ -11,9 +12,12 @@ class App {
     });
   }
 
-  public listen(port: number) {
-    return startStandaloneServer(this.server, {
-      listen: { port }
+  public listen(port: number, db: Connection) {
+    return startStandaloneServer<any>(this.server, {
+      listen: { port },
+      context: ({ req, res }) => {
+        return Promise.resolve(db);
+      }
     }).then(() => {
       console.log(`Listening on port: ${port}`);
     });
